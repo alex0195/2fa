7df0a26e177be3b8b271c1b4224fa8c2f6c13bd0
Alexander <alex0195@gmail.com>
2020-08-11 19:02:18 -0400

    Autenticacion 2fa

---
+++ app.js
@@ -0,0 +1,43 @@
+const Express = require("express");
+
+const Speakeasy = require("speakeasy");
+
+var app = Express();
+
+app.use(Express.urlencoded({ extended: true }));
+app.use(Express.json());
+
+app.get("/", (request, response, next) => {
+    //var secret = Speakeasy.generateSecret({ length: 20 });
+    response.send({ "secret": 'OQSWI623IBGUSP3LHYXDG2DLJAUW6KCI' });
+});
+
+app.post("/totp-secret", (request, response, next) => {
+    var secret = Speakeasy.generateSecret({ length: 20 });
+    response.send({ "secret": secret.base32 });
+});
+
+app.post("/totp-generate", (request, response, next) => {
+    response.send({
+        "token": Speakeasy.totp({
+            secret: request.body.secret,
+            encoding: "base32"
+        }),
+        "remaining": (30 - Math.floor((new Date()).getTime() / 1000.0 % 30))
+    });
+});
+
+app.post("/totp-validate", (request, response, next) => {
+    response.send({
+        "valid": Speakeasy.totp.verify({
+            secret: request.body.secret,
+            encoding: "base32",
+            token: request.body.token,
+            window: 0
+        })
+    });
+});
+
+app.listen(3002, () => {
+    console.log("Listening at :3002...");
+});
